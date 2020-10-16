e2ebc608212b0b981ffd2f57f9d9fb7889993231
Rafsanjani Aziz <coded_raf@yahoo.com>
2020-10-16 19:55:07 +0100

    Added more implmenetation details

--- app/src/main/java/com/musavili/application/mobileopen10/LoginFragment.java
+++ app/src/main/java/com/musavili/application/mobileopen10/LoginFragment.java
@@ -1,10 +1,13 @@
 package com.musavili.application.mobileopen10;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -13,5 +16,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class LoginFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        return super.onContextItemSelected(item);
+    }
 }
