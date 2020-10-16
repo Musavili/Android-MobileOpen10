b243fa302e9a8ca3cf35b19379b2019e365a0c31
Rafsanjani Aziz <coded_raf@yahoo.com>
2020-10-16 19:51:03 +0100

    Created login screen with implementation

---
+++ app/src/main/java/com/musavili/application/mobileopen10/LoginFragment.java
@@ -0,0 +1,17 @@
+package com.musavili.application.mobileopen10;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginFragment extends Fragment {
+
+}
