70c1f0e68ea687dd6c46375ccd6f52883c89f077
Musavili <59392019+Musavili@users.noreply.github.com>
2020-10-28 16:56:03 +0300

    chat

---
+++ app/src/main/java/com/musavili/application/mobileopen10/util/Util.java
@@ -0,0 +1,31 @@
+package com.musavili.application.mobileopen10.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+import com.google.firebase.storage.FirebaseStorage;
+
+public class Util {
+    private static FirebaseStorage reference = FirebaseStorage.getInstance();
+    public static final String URL_STORAGE_REFERENCE = "gs://mobileopen10-2c5d2.appspot.com";
+    public static final String FOLDER_STORAGE_IMG = "images";
+
+    public static void initToast(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public  static boolean checkConnection(Context context) {
+        boolean conectado;
+        ConnectivityManager conectivtyManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        conectado = conectivtyManager.getActiveNetworkInfo() != null
+                && conectivtyManager.getActiveNetworkInfo().isAvailable()
+                && conectivtyManager.getActiveNetworkInfo().isConnected();
+        return conectado;
+    }
+
+    public static String local(String latitudeFinal,String longitudeFinal){
+        return "https://maps.googleapis.com/maps/api/staticmap?center="+latitudeFinal+","+longitudeFinal+"&zoom=18&size=280x280&markers=color:red|"+latitudeFinal+","+longitudeFinal;
+    }
+
+}
